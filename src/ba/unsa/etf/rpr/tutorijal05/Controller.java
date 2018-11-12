f20ee538e0fb82f62b0ee2e28ae6eb7afd91d20f
imaticevic1 <imaticevic1@etf.unsa.ba>
2018-11-12 23:57:34 +0100

    napisao program u java kodu i problem kako testirati kod unutar klase kontroler

--- src/ba/unsa/etf/rpr/tutorijal05/Controller.java
+++ src/ba/unsa/etf/rpr/tutorijal05/Controller.java
@@ -1,5 +1,22 @@
 package ba.unsa.etf.rpr.tutorijal05;
 
-public class Controller {
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
+
+import java.awt.event.ActionEvent;
+
+public class Controller{
+    private Button posto, podeljeno, zbrajanje, mnozenje, oduzimanje;
+    private Button nula, broj1, broj2, broj3, broj4, broj5, broj6, broj7, broj8, broj9;
+    private TextField poljeTeksta;
+
+    public void handleButtonAction(ActionEvent event) {
+        if (event.getSource() == posto) {
+            poljeTeksta.setText("%");
+        }
+        else if (event.getSource() == posto) {
+            poljeTeksta.setText("%");
+        }
+    }
 }
